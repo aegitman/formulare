00a69e2ff25650e733899d3e45b145c907a02159
Alex Gitman <alex.gitman@pm.me>
2025-01-25 22:09:06 +0100

    first rectification

--- js/itl-054-2016-objs.js
+++ js/itl-054-2016-objs.js
@@ -2,7 +2,7 @@ const {PDFDocument, rgb, StandardFonts } = PDFLib;
 
 export async function modifyPdf(fieldsMap) {
   // Fetch an existing PDF document
-  const url = "ITL-054.pdf";
+  const url = "pdfs/ITL-054.pdf";
   const existingPdfBytes = await fetch(url).then((res) =>
     res.arrayBuffer()
   );
